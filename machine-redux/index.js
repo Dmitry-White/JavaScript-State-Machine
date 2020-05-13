2f9f9f4b477352ccb95e9802479b140a33942818
Dmitry White <dmitry.b.white@gmail.com>
2020-05-13 18:39:59 +0300

    Add retry button

--- machine-redux/index.js
+++ machine-redux/index.js
@@ -1,2 +1,5 @@
 const clickButton = document.querySelector("#click-button");
+const retryButton = document.querySelector("#retry-button");
+
 clickButton.addEventListener("click", () => machine.dispatch("click"));
+retryButton.addEventListener("click", () => machine.dispatch("retry"));
