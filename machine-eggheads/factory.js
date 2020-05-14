7230a82b5f57188fb41ba82e975afe60a896d76b
Dmitry White <dmitry.b.white@gmail.com>
2020-05-14 17:34:20 +0300

    Add transition logic

--- machine-eggheads/factory.js
+++ machine-eggheads/factory.js
@@ -2,6 +2,23 @@ const createMachine = (stateMachineDefinition) => {
   const machine = {
     value: stateMachineDefinition.initialState,
     transition(currentState, event) {
+      const currentStateDefinition = stateMachineDefinition[currentState];
+      const destinationTransition = currentStateDefinition.transitions[event];
+
+      if (!destinationTransition) {
+        return;
+      }
+
+      const destinationState = destinationTransition.target;
+      const destinationStateDefinition =
+        stateMachineDefinition[destinationState];
+
+      currentStateDefinition.actions.onExit();
+      destinationTransition.action();
+      destinationStateDefinition.actions.onEnter();
+
+      this.value = destinationState;
+
       return this.value;
     },
   };
