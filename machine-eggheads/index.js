1e1752f13baf484f40a362b3e6fc91db61a04c7d
Dmitry White <dmitry.b.white@gmail.com>
2020-05-14 17:35:19 +0300

    Fix states

--- machine-eggheads/index.js
+++ machine-eggheads/index.js
@@ -3,8 +3,8 @@ const machine = require("./machine");
 const initialState = machine.value;
 console.log(`current state: ${initialState}`); // current state: off
 
-const onState = machine.transition(state, "switch");
+const onState = machine.transition(initialState, "switch");
 console.log(`current state: ${onState}`); // current state: on
 
-const offState = machine.transition(state, "switch");
+const offState = machine.transition(onState, "switch");
 console.log(`current state: ${offState}`); // current state: off
