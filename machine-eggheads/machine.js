4a5b212fc9463c5e75a0d4cba789bb4d16e4b8de
Dmitry White <dmitry.b.white@gmail.com>
2020-05-14 17:00:17 +0300

    Add states and actions

--- machine-eggheads/machine.js
+++ machine-eggheads/machine.js
@@ -1,1 +1,23 @@
-const machine = createMachine({});
+const machine = createMachine({
+  initialState: "off",
+  on: {
+    actions: {
+      onEnter() {
+        console.log("on: onEnter");
+      },
+      onExit() {
+        console.log("on: onExit");
+      },
+    },
+  },
+  off: {
+    actions: {
+      onEnter() {
+        console.log("off: onEnter");
+      },
+      onExit() {
+        console.log("off: onExit");
+      },
+    },
+  },
+});
