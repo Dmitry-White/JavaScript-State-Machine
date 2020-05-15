44c0b10878ab7a429b0eadb11ce92bc203707c3c
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-05-15 18:52:00 +0300

    Add transition method to App

--- machine-bellah-payment/src/App.js
+++ machine-bellah-payment/src/App.js
@@ -1,49 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import stateMachine from './machine';
 
 import './App.css';
 
-console.log(stateMachine.initialState);
+class App extends Component {
+  componentDidMount() {
+    this.setState({
+      machine: stateMachine.initialState
+    });
+  };
 
-const App = () => {
-  return (
-    <div className="form-container">
-      <div className="form-header">
-        <h2>State Machine Payment Form</h2>
-      </div>
+  runActions = (state) => {
+    if (state.actions.length > 0) {
+      state.actions.forEach(f => this[f]());
+    }
+  }
 
-      <div className="form-body">
-        <form>
-          <div className="form-group">
-            <label htmlFor="NameOnCard">Name on card</label>
-            <input
-              id="NameOnCard"
-              className="form-control"
-              type="text"
-              maxLength="255"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="CreditCardNumber">Card number</label>
-            <input
-              id="CreditCardNumber"
-              className="null card-image form-control"
-              type="text"
-            />
-          </div>
-          <button
-            id="PayButton"
-            className="btn btn-block btn-success submit-button"
-            type="submit"
-          >
-            <span className="submit-button-lock" />
-            <span className="align-middle">Pay Now</span>
-          </button>
-        </form>
+  transition = (eventType, extState) => {
+    const newState = stateMachine.transition(
+      this.state.machine.value,
+      eventType,
+      {
+        state: this.state
+      }
+    );
+
+    this.runActions(newState);
+
+    this.setState({
+      machine: newState,
+      msg: extState && extState.msg ? extState.msg : ""
+    });
+  }
+
+  render() {
+    return (
+      <div className="form-container">
+        <div className="form-header">
+          <h2>State Machine Payment Form</h2>
+        </div>
+
+        <div className="form-body">
+          <form>
+            <div className="form-group">
+              <label htmlFor="NameOnCard">Name on card</label>
+              <input
+                id="NameOnCard"
+                className="form-control"
+                type="text"
+                maxLength="255"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="CreditCardNumber">Card number</label>
+              <input
+                id="CreditCardNumber"
+                className="null card-image form-control"
+                type="text"
+              />
+            </div>
+            <button
+              id="PayButton"
+              className="btn btn-block btn-success submit-button"
+              type="submit"
+            >
+              <span className="submit-button-lock" />
+              <span className="align-middle">Pay Now</span>
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+}
 
 export default App;
