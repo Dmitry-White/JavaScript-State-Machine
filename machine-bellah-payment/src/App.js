82a190fb08a8633586752e28be48c9fc4e600dc0
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-05-15 18:30:45 +0300

    Change App component

--- machine-bellah-payment/src/App.js
+++ machine-bellah-payment/src/App.js
@@ -1,31 +1,49 @@
 import React from 'react';
 
 import stateMachine from './machine';
-import logo from './logo.svg';
 
 import './App.css';
 
 console.log(stateMachine.initialState);
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <div className="form-header">
+        <h2>State Machine Payment Form</h2>
+      </div>
+
+      <div className="form-body">
+        <form>
+          <div className="form-group">
+            <label htmlFor="NameOnCard">Name on card</label>
+            <input
+              id="NameOnCard"
+              className="form-control"
+              type="text"
+              maxLength="255"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="CreditCardNumber">Card number</label>
+            <input
+              id="CreditCardNumber"
+              className="null card-image form-control"
+              type="text"
+            />
+          </div>
+          <button
+            id="PayButton"
+            className="btn btn-block btn-success submit-button"
+            type="submit"
+          >
+            <span className="submit-button-lock" />
+            <span className="align-middle">Pay Now</span>
+          </button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
