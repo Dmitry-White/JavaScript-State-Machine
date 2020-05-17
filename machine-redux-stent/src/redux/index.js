f5cdb99db32b431cf31955fbd5fcb8a62bd14ce6
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-05-17 22:19:20 +0300

    Add redux saga

--- machine-redux-stent/src/redux/index.js
+++ machine-redux-stent/src/redux/index.js
@@ -1,10 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import reducer from './reducer';
+import saga from './saga';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(sagaMiddleware))
 );
+
+sagaMiddleware.run(saga);
 
 export default store;
