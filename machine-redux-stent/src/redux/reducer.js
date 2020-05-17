7e087e9630d71f0ac8c9094ed000a61c406de6f4
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-05-17 20:15:24 +0300

    Add reducer

---
+++ machine-redux-stent/src/redux/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  user: null,
+  error: null,
+  loading: false,
+  credentials: null
+}
+
+const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+}
+
+export default reducer;
